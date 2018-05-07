793628cbb983863efce12d694d6c5dcae77220ae
Waleed al-Hubaishi <waleed.alhubaishi@students.fhnw.ch>
2018-05-07 17:42:18 +0200

    add the missing screens to the project

---
+++ BeFit/BeFit/StatsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StatsTableViewCell.swift
+//  BeFit
+//
+//  Created by Waleed al-Hubaishi on 07.05.18.
+//  Copyright © 2018 Waleed al-Hubaishi. All rights reserved.
+//
+
+import UIKit
+
+class StatsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
